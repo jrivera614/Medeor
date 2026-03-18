bdb0b5bdbb96beb751f9846f927b8613986b58e3
jrivera614 <jrivera614@gmail.com>
2026-03-18 07:48:55 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,1 +1,9 @@
+'use client';
 
+import dynamic from 'next/dynamic';
+
+const TCCCApp = dynamic(() => import('./TCCCApp'), { ssr: false });
+
+export default function Home() {
+  return <TCCCApp />;
+}
